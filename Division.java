8f15f3dfd10a061ac6f01704112530aa8edef321
Nimal <nimalraj.2001133@srec.ac.in>
2024-08-08 17:28:33 +0530

    +div

---
+++ Division.java
@@ -0,0 +1,10 @@
+public class Division {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 6;
+        int c=a/b;
+        System.out.println(c);
+        System.out.println("Thank You");
+
+    }
+}
